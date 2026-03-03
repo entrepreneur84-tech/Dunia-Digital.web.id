e177d9f72a9ae8e517b660fa054cc9a75439a4a9
Entrepreneur <entrepreneur84@bk.ru>
2026-03-04 02:32:39 +0700

    Update mailTemplate.js

--- workers/utils/mailTemplate.js
+++ workers/utils/mailTemplate.js
@@ -1,1 +1,20 @@
 
+export function buildEmail({ email, product, license, link }) {
+  return {
+    to: email,
+    subject: "Ebook Anda: " + product,
+    body: `
+Terima kasih telah membeli ebook ${product}
+
+LICENSE: ${license}
+
+Link akses:
+${link}
+
+Jangan bagikan license ini kepada orang lain.
+
+Salam,
+Dunia Digital
+`
+  }
+}
