4f6991d5a4f13e8c851f663b867856cc854d349d
Entrepreneur <entrepreneur84@bk.ru>
2026-03-04 02:30:46 +0700

    Update licenses.js

--- workers/kv/licenses.js
+++ workers/kv/licenses.js
@@ -1,1 +1,9 @@
+export async function saveLicense(env, license) {
+  await env.LICENSES.put(license.license, JSON.stringify(license))
+}
 
+export async function getLicense(env, key) {
+  const data = await env.LICENSES.get(key)
+  if (!data) return null
+  return JSON.parse(data)
+}
