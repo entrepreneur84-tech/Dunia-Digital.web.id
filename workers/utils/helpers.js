bf14fdbc21b6892f9d665b047cfdc60c155efdbb
Entrepreneur <entrepreneur84@bk.ru>
2026-03-04 02:28:22 +0700

    Update helpers.js

--- workers/utils/helpers.js
+++ workers/utils/helpers.js
@@ -1,1 +1,7 @@
+export function generateInvoice() {
+  return "INV-" + Date.now() + "-" + Math.floor(Math.random() * 9999)
+}
 
+export function generateLicense() {
+  return "LIC-" + Math.random().toString(36).substring(2, 10).toUpperCase()
+}
