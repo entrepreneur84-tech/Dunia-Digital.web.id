8b4adfd63be1a73ba8058fea61234d6fb7f9b950
Entrepreneur <entrepreneur84@bk.ru>
2026-03-04 02:34:20 +0700

    Update waTemplate.js

--- workers/utils/waTemplate.js
+++ workers/utils/waTemplate.js
@@ -1,1 +1,11 @@
+export function buildWA({ phone, product, license, link }) {
+  const msg = `Terima kasih telah membeli ${product}
 
+LICENSE: ${license}
+
+Akses ebook:
+${link}
+
+Support: admin@dunia-digital.web.id`
+  return "https://wa.me/"6285175313909"?text=" + encodeURIComponent(msg)
+}
