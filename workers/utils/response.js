90b0243b03a064f9515cfcc3172a6b950bc2dfd0
Entrepreneur <entrepreneur84@bk.ru>
2026-03-04 02:29:16 +0700

    Update response.js

--- workers/utils/response.js
+++ workers/utils/response.js
@@ -1,1 +1,10 @@
+export function json(data, status = 200) {
+  return new Response(JSON.stringify(data, null, 2), {
+    status,
+    headers: { "Content-Type": "application/json" }
+  })
+}
 
+export function text(data, status = 200) {
+  return new Response(data, { status })
+}
