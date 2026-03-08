7141040ffcbf5ec2e2b0ab022542be29689e60ac
Entrepreneur <entrepreneur84@bk.ru>
2026-03-09 00:41:38 +0700

    Update download.js

--- workers/routes/download.js
+++ workers/routes/download.js
@@ -1,1 +1,23 @@
+export async function handleDownload(request, env) {
 
+ const url = new URL(request.url)
+
+ const fileId = url.searchParams.get("file")
+
+ if (!fileId) {
+  return new Response("File not found", { status: 404 })
+ }
+
+ const file = await env.DOWNLOADS.get(fileId, "arrayBuffer")
+
+ if (!file) {
+  return new Response("Ebook not found", { status: 404 })
+ }
+
+ return new Response(file, {
+  headers: {
+   "Content-Type": "application/pdf"
+  }
+ })
+
+}
