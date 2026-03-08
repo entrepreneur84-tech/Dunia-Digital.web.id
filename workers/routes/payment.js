820ccad301e68900292cb94db83bca9926c7beb7
Entrepreneur <entrepreneur84@bk.ru>
2026-03-09 00:40:52 +0700

    Update payment.js

--- workers/routes/payment.js
+++ workers/routes/payment.js
@@ -1,1 +1,28 @@
+export async function handlePaymentInfo() {
 
+ const payment = {
+
+  seabank: {
+   bank: "SeaBank",
+   name: "AHMAD BARIZI",
+   account: "901981495649"
+  },
+
+  jago: {
+   bank: "Bank Jago",
+   name: "Ahmad Barizi",
+   account: "109896731184"
+  },
+
+  confirm: {
+   whatsapp: "6285175313909",
+   email: "admin@dunia-digital.web.id"
+  }
+
+ }
+
+ return new Response(JSON.stringify(payment), {
+  headers: { "Content-Type": "application/json" }
+ })
+
+}
